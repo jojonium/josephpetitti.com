6b9f1c6341d23284ac74e874ff9ed82a8687f568
jojonium <37883763+jojonium@users.noreply.github.com>
2018-03-28 17:25:04 -0400

    Added site files

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/cowboy.jpg') {
+		myImage.setAttribute('src', 'images/grounded.jpg');
+	} else {
+		myImage.setAttribute('src', 'images/cowboy.jpg');
+	}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Welcome, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Welcome, ' + storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
