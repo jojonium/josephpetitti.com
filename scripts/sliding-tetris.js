7b51bc9cd5846d95000a7b5fbc9c6d1ed743837c
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2019-04-16 12:40:34 -0400

    add addLine

--- scripts/sliding-tetris.js
+++ scripts/sliding-tetris.js
@@ -2,6 +2,7 @@
 
 /* globals */
 var PIECE_COUNTER = 0;
+var MOVES = 0;
 const SQUARE_SIZE = 60;
 const B_HEIGHT = 10;
 const B_WIDTH = 8;
@@ -35,12 +36,6 @@ const Piece = function(x = 0, y = 0, width = 1, height = 1) {
 	this.id = PIECE_COUNTER++;
 	this.color = randColor();
 
-	// update BOARD
-	for (let i = 0; i < width; ++i) {
-		for (let j = 0; j < height; ++j) {
-			BOARD[i + x][j + y] = this.id;
-		}
-	}
 };
 
 
@@ -66,17 +61,20 @@ Piece.prototype = {
 				axis: 'x',
 				containment: "parent",
 				opacity: 0.75,
-				/*start: function() {
-					this.clearPosition();
-				}.bind(this),*/
 				stop: function() {
-					this.updatePosition();
+					// disable dragging while the animations are playing
+					$('.piece').draggable('option', 'disabled', true);
+					MOVES++;
+					this.updatePositionByDOM();
 				}.bind(this),
 				drag: function(event, ui) {
 					this.useWalls(ui);
 				}.bind(this)
 			}
 		}));
+
+		// update BOARD
+		this.updatePosition();
 
 		return this;
 	},
@@ -109,7 +107,7 @@ Piece.prototype = {
 	/**
 	 * Clears the old position of the Piece in BOARD.
 	 *
-	 * Returns the object so it can be changed
+	 * Returns the object so it can be chained
 	 **/
 	clearPosition: function() {
 		for (let i = this.x; i < this.width + this.x; ++i) {
@@ -117,6 +115,23 @@ Piece.prototype = {
 				BOARD[i][j] = -1;
 			}
 		}
+
+		return this;
+	},
+
+	/**
+	 * Sets the position of the Piece in BOARD.
+	 *
+	 * Returns the object so it can be chained
+	 **/
+	updatePosition: function() {
+		for (let i = this.x; i < this.width + this.x; ++i) {
+			for (let j = this.y; j < this.height + this.y; ++j) {
+				BOARD[i][j] = this.id;
+			}
+		}
+
+		return this;
 	},
 
 	/** 
@@ -126,7 +141,7 @@ Piece.prototype = {
 	 *
 	 * Returns the object so it can be chained.
 	 **/
-	updatePosition: function() {
+	updatePositionByDOM: function() {
 		// clear the original position
 		this.clearPosition();
 
@@ -177,19 +192,194 @@ Piece.prototype = {
 			.css('top', (B_HEIGHT - this.y - this.height) * SQUARE_SIZE);
 
 		return fell;
+	},
+
+	/**
+	 * Destroys this piece object and removes it from the board
+	 **/
+	destroy: function() {
+		this.clearPosition();
+		P[this.id] = undefined;
+		$('#piece-' + this.id).remove();
 	}
 };
 
 
+/**
+ * Once the animations are done playing one turned has passed, so update the
+ * move counter, check to see if you can clear any lines, and re-enable
+ * dragging.
+ **/
+const turnDone = () => {
+	$('#moves').html(MOVES);
+	// check for clearable lines
+	let line = -1;
+	let cleared = false;
+	for (let i = 0; i < B_HEIGHT; ++i) {
+		line = i;
+		for (let j = 0; j < B_WIDTH; ++j) {
+			if (BOARD[j][i] == -1) {
+				line = -1;
+				break;
+			}
+		}
+		if (line != -1) {
+			clearLine(line);
+			cleared = true;
+		}
+	}
+	if (!cleared)
+		prepareForNextTurn();
+};
+
+
+const prepareForNextTurn = () => {
+	setTimeout(() => {
+		$('.piece').draggable('option', 'disabled', false);
+	}, 500);
+}
+
+
+/**
+ * Clears a filled line
+ **/
+const clearLine = (n) => {
+	$('.piece').draggable('option', 'disabled', true);
+	let blocksToClear = {};
+	for (let i = 0; i < B_WIDTH; ++i) {
+		blocksToClear[BOARD[i][n]] = P[BOARD[i][n]];
+	}
+	for (p in blocksToClear) {
+		if (blocksToClear[p]) {
+			$('#piece-' + p).css({
+				'background-color': 'white',
+				'box-shadow': '0 0 10px 10px white',
+				'z-index': 1
+			});
+		}
+	}
+	setTimeout(function() {
+		for (p in blocksToClear) {
+			if (P[p])
+				P[p].destroy();
+		}
+		applyGravityAll();
+	}, 1000);
+
+};
+
+/**
+ * Adds a line to the bottom of the board, moving all other lines up.
+ *
+ * lvl: the difficulty level of this line, from 1 to 4
+ **/
+const addLine = (lvl = 1) => {
+	// don't go too fast
+	if ($('.piece').draggable('option', 'disabled')) {
+		console.log('whoa, slow down!');
+		return;
+	}
+
+	$('.piece').draggable('option', 'disabled', true);
+	let probs;
+	switch (lvl) {
+		case 1: probs = [.5, .4, .1, 0, 0]; break;
+		case 2: probs = [.4, .3, .2, .1, 0]; break;
+		case 3: probs = [.3, .2, .2, .2, .1]; break;
+		case 4: probs = [.2, .3, .2, .15, .15]; break;
+		case 5: probs = [.1, .2, .3, .2, .2]; break;
+		default: probs = [.5, .4, .1, 0, 0];
+	}
+	var roll, target, blockWidth;
+	var blocksToAdd = {};
+	var remPos = new Array(B_WIDTH);
+	for (let r = 0; r < B_WIDTH; ++r) remPos[r] = -1;
+
+	var i = 0;
+	// generate blocks
+	outerloop: while (i < B_WIDTH) {
+		blockWidth = 0;
+		target = 0;
+
+		roll = Math.random();
+		for (let j = 0; j < 4; ++j) {
+			if (roll < target + probs[j]) {
+				blockWidth = j;
+				break;
+			} else {
+				target += probs[j];
+			}
+		}
+
+		// this space is empty
+		if (blockWidth == 0) {
+			++i;
+			continue;
+		}
+
+		// check whether this would fit here
+		innerloop: for (let j = i; j < i + blockWidth; ++j) {
+			if (j >= B_WIDTH || remPos[j] != -1) {
+				// doesn't fit
+				++i;
+				continue outerloop;
+			}
+		}
+
+		// record this position for later
+		for (let j = i; j < i + blockWidth - 1; ++j) {
+			remPos[j] = 1;
+		}
+
+		var temp =  new Piece(i, 0, blockWidth);
+		blocksToAdd[temp.id] = temp;
+		i += blockWidth;
+	}
+
+	// move other blocks up
+	for (let p in P) {
+		if (P[p]) {
+			P[p].clearPosition();
+			P[p].y++;
+		}
+	}
+	for (let p in P) {
+		if (P[p]) {
+			P[p].updatePosition();
+			let newTop = Number($('#piece-' + P[p].id).css('top').slice(0, -2)) 
+				- SQUARE_SIZE;
+			$('#piece-' + P[p].id).css('top', newTop);
+		}
+	}
+
+	// add blocksToAdd
+	for (let b in blocksToAdd) {
+		setTimeout(() => {
+			P[b] = blocksToAdd[b].add();
+		}, 500);
+	}
+
+	// apply gravity
+	setTimeout(() => { applyGravityAll(); }, 501);
+};
+
+/**
+ * Calls applyGravity() on all pieces on the board
+ **/
 const applyGravityAll = () => {
+	$('.piece').draggable('option', 'disabled', true);
 	let fell = false;
 	for (p in P) {
-		if (P[p].applyGravity())
-			fell = true;
+		if (P[p]) {
+			if (P[p].applyGravity())
+				fell = true;
+		}
 	}
 	if (fell)
 		applyGravityAll();
-}
+	else
+		turnDone();
+};
 
 
 /**
@@ -212,11 +402,11 @@ const randColor = () => {
  **/
 const init = () => {
 	P[0] = new Piece().add();
-	P[1] = new Piece(0, 1).add();
-	P[2] = new Piece(0, 2, 3).add();
-	P[3] = new Piece(2, 3, 3).add();
-	P[4] = new Piece(0, 3, 1).add();
-	P[5] = new Piece(5, 0, 1).add();
+	P[1] = new Piece(2, 0, 6).add();
+	P[2] = new Piece(0, 1, 2).add();
+	P[3] = new Piece(2, 1, 3).add();
+	P[4] = new Piece(1, 2).add();
+	P[5] = new Piece(2, 2, 3).add();
 };
 
 
@@ -234,6 +424,9 @@ $('document').ready(() => {
 			BOARD[i][j] = -1;
 		}
 	}
+
+	// prepare #moves
+	$('#moves').html(MOVES);
 
 	init();
 });
