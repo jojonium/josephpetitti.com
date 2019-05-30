ba2bd7cba37602e032a52fb98277ddb6117b6345
Joseph Petitti <tarrasque9@gmail.com>
2019-05-30 16:28:57 -0400

    updated blogify script

--- blog/md2blog.sh
+++ blog/md2blog.sh
@@ -4,7 +4,7 @@ if [ $# -lt 5 ]; then
    echo "Usage: ./md2blog title description image url markdown"
    exit 1
 else
-	echo '<!doctype html>' >> $4.html
+	echo '<!doctype html>' > $4.html
 	echo '  <head>' >> $4.html
 	echo '    <meta http-equiv="content-type" content="text/html; charset=utf-8" />' >> $4.html
 	echo '    <title>'$1'</title>' >> $4.html
@@ -14,9 +14,11 @@ else
 	echo '    <meta property="og:image" content="https://josephpetitti.com/blog/images/'$3'" />' >> $4.html
 	echo '    <meta property="og:type" content="article" />' >> $4.html
 	echo '    <meta property="og:url" content="https://josephpetitti.com/blog/'$4'" />' >> $4.html
-	echo '    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, width=device-width, user-scalable=no" />' >> $4.html
-	cat template-top.html >> $4.html
+	awk 'NR >= 12 && NR <= 34' template.html >> $4.html
 	echo '      <span class="date"><time datetime="'$(date +%Y-%m-%d)'">'$(date +%B)' '$(date +%-d)', '$(date +%Y)'</span>' >> $4.html
 	echo '' >> $4.html
+	echo '      <h1 class="title">'$1'</h1>' >> $4.html
+	echo '' >> $4.html
 	pandoc -f markdown -t html $5 | sed 's/^/      /' >> $4.html
+	tail -n 22 template.html >> $4.html
 fi
