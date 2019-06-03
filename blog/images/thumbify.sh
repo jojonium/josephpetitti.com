19f45f2b47bcc445f5d3d6a3160dc5608c145bc3
Joseph Petitti <tarrasque9@gmail.com>
2019-06-03 15:16:35 -0400

    add usage and multi-arg support

--- blog/images/thumbify.sh
+++ blog/images/thumbify.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 
-convert -thumbnail 600 $1 th_$1;
+# Creates a thumbnail of each argument with a width of 600 pixels.
+# If the input file has the filename image.png, the output thumbnail will have
+# the filename th_image.png
+
+# check number of arguments
+if [ "$#" -lt 1 ]; then
+	echo "Usage: ./thumbify.sh file ...";
+	exit 1;
+fi
+
+for arg in "$@"
+do 
+	convert -thumbnail 600 $arg th_$arg
+done
+
