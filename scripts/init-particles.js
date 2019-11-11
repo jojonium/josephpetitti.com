cb442179fd2bd0289ad45aef380d74b7342754aa
Joseph Petitti <jppetitti@wpi.edu>
2019-11-11 14:36:49 -0500

    switch to snow

--- scripts/init-particles.js
+++ scripts/init-particles.js
@@ -1,6 +1,6 @@
 /* (C) 2019 Joseph Petitti | https://josephpetitti.com/license.txt */
 
 window.onload = function() {
-  particlesJS.load('particles-js', 'https://' + document.domain + '/assets/particle-cfg.json');
+  particlesJS.load('particles-js', 'https://' + document.domain + '/assets/snow-cfg.json');
 }
 fadein();
