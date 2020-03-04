878d785c239bd2810e9615acefe54eb215f9dd51
Joseph Petitti <jppetitti@wpi.edu>
2020-03-04 02:28:38 -0500

    switch from snow to graph particles

--- scripts/init-particles.js
+++ scripts/init-particles.js
@@ -1,6 +1,9 @@
-/* (C) 2019 Joseph Petitti | https://josephpetitti.com/license.txt */
+/* (C) 2019, 2020 Joseph Petitti | https://josephpetitti.com/license.txt */
 
 window.onload = function() {
-  particlesJS.load('particles-js', 'https://' + document.domain + '/assets/snow-cfg.json');
-}
+  particlesJS.load(
+    "particles-js",
+    "https://" + document.domain + "/assets/particle-cfg.json"
+  );
+};
 fadein();
