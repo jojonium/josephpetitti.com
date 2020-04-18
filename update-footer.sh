df3d6c5c7039f66892af295254970d1b35da19cf
Joseph Petitti <jppetitti@wpi.edu>
2020-04-18 19:49:10 -0400

    add noreferrer noopener to footer links

--- update-footer.sh
+++ update-footer.sh
@@ -2,4 +2,4 @@
 
 # Replaces anything between <!-- Footer -- and </footer> with the contents of
 # footer.txt for all html files in the current directory
-perl -i -p0e 's/<!-- Footer --(.|\n)*<\/footer>/`cat footer.txt`/se' *.html
+perl -i -p0e 's/<!-- Footer --(.|\n)*<\/footer>\n/`cat footer.txt`/se' *.html
