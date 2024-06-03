59369efc2b6d26a9302b0ad411f729b4d0cc02e2
Joseph Petitti <joseph@petitti.org>
2024-06-03 13:17:32 -0400

    Update leaflet to 1.9.4

--- scripts/hong-kong.js
+++ scripts/hong-kong.js
@@ -1,21 +1,33 @@
-/* (C) 2019 Joseph Petitti | https://josephpetitti.com/license.txt */
+/* (C) 2019, 2024 Joseph Petitti | https://josephpetitti.com/license.txt */
 
-if (L)
+if (L) {
   document.getElementById('mapDiv').innerHTML = '';
-var mymap = L.map('mapDiv').setView([22.282, 114.154], 16);
-      L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: `&copy;
-                <a href="https://www.openstreetmap.org/copyright">
-                    OpenStreetMap
-                </a>`,
-    subdomains: ['a','b','c']
-  }
-).addTo( mymap );
+  const myMap = L.map('mapDiv').setView([22.282, 114.154], 16);
 
-var triangle = L.polygon([
-  [22.28366, 114.15095],
-  [22.27983, 114.15420],
-  [22.28463, 114.15649]
-]).addTo(mymap);
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; ' +
+        '<a href="https://www.openstreetmap.org/copyright" ' +
+        'target="_blank" rel="noreferrer noopener">OpenStreetMap</a>',
+      subdomains: ['a', 'b', 'c']
+    }
+  ).addTo(myMap);
+
+  L.polygon([
+    [22.28366, 114.15095],
+    [22.27983, 114.15420],
+    [22.28463, 114.15649]
+  ]).addTo(myMap);
+
+  myMap.attributionControl.setPrefix(
+    '<a href="https://leafletjs.com" target="_blank" ' +
+    'rel="noreferrer noopener" ' +
+    'title="A JavaScript library for interactive maps">Leaflet</a>'
+  );
+
+
+} else {
+  console.error("Failed to use Leaflet. Is it loaded properly?");
+}
+
