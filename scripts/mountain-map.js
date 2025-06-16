1f449da929ecf40bb096cb0f473f5d9b2c87b1c7
Joseph Petitti <joseph@petitti.org>
2025-06-16 17:11:48 -0400

    Add Adams via King Ravine

--- scripts/mountain-map.js
+++ scripts/mountain-map.js
@@ -77,7 +77,7 @@ window.addEventListener("load", () => {
   const hikingTracks = L.layerGroup();
   const paddlingTracks = L.layerGroup();
 
-  fetch("/assets/hiking.json?v=23")
+  fetch("/assets/hiking.json?v=24")
     .then(response => response.json())
     .then(data => {
       for (const {points, name} of data) {
