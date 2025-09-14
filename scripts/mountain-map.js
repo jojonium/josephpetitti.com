0d97d22beb0b25821a5023c4e18b0ed755ae9086
Joseph Petitti <joseph@petitti.org>
2025-09-14 16:24:25 -0400

    Add Southern Presidentials via Dry River

--- scripts/mountain-map.js
+++ scripts/mountain-map.js
@@ -77,7 +77,7 @@ window.addEventListener("load", () => {
   const hikingTracks = L.layerGroup();
   const paddlingTracks = L.layerGroup();
 
-  fetch("/assets/hiking.json?v=31")
+  fetch("/assets/hiking.json?v=32")
     .then(response => response.json())
     .then(data => {
       for (const {points, name} of data) {
