439cfd873a6238a689feab6c689305f085396ade
Joseph Petitti <joseph@petitti.org>
2025-10-01 19:29:22 -0400

    Fix typo

--- scripts/mountain-map.js
+++ scripts/mountain-map.js
@@ -77,7 +77,7 @@ window.addEventListener("load", () => {
   const hikingTracks = L.layerGroup();
   const paddlingTracks = L.layerGroup();
 
-  fetch("/assets/hiking.json?v=33")
+  fetch("/assets/hiking.json?v=34")
     .then(response => response.json())
     .then(data => {
       for (const {points, name} of data) {
