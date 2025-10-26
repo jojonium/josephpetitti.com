7a76c223a21431a39c18a88514609c43024bd089
Joseph Petitti <joseph@petitti.org>
2025-10-26 18:10:29 -0400

    Add 2025 Wildcats

--- scripts/mountain-map.js
+++ scripts/mountain-map.js
@@ -77,7 +77,7 @@ window.addEventListener("load", () => {
   const hikingTracks = L.layerGroup();
   const paddlingTracks = L.layerGroup();
 
-  fetch("/assets/hiking.json?v=36")
+  fetch("/assets/hiking.json?v=37")
     .then(response => response.json())
     .then(data => {
       for (const {points, name} of data) {
