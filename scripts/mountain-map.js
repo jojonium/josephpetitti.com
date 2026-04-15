df46c4fd469ea34f04630341134122e1f5bdf8a0
Joseph Petitti <joseph@petitti.org>
2026-04-15 16:39:20 -0400

    Add 2026 April Kinsmans

--- scripts/mountain-map.js
+++ scripts/mountain-map.js
@@ -77,7 +77,7 @@ window.addEventListener("load", () => {
   const hikingTracks = L.layerGroup();
   const paddlingTracks = L.layerGroup();
 
-  fetch("/assets/hiking.json?v=46")
+  fetch("/assets/hiking.json?v=47")
     .then(response => response.json())
     .then(data => {
       for (const {points, name} of data) {
